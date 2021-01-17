c50b422f5a1904632ed608f041cee3701399daa2
Piyush <piyush41y08h@gmail.com>
2021-01-17 11:22:27 +0530

    Fix client path

--- services/environment.js
+++ services/environment.js
@@ -28,10 +28,10 @@ function development(app) {
 /** Environment based config for production */
 function production(app) {
   if (process.env.NODE_ENV === "production") {
-    app.use(express.static("client/build"));
+    app.use(express.static("../client/build"));
 
     app.get("*", (req, res) => {
-      res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+      res.sendFile(path.resolve("../client/build/index.html"));
     });
   }
 }
