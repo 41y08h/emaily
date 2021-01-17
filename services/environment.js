c0867f9b155c7781f5c1652342d9990616d44800
Piyush <piyush41y08h@gmail.com>
2021-01-17 12:40:41 +0530

    Fix env config

--- services/environment.js
+++ services/environment.js
@@ -28,7 +28,7 @@ function development(app) {
 
 /** Environment based config for production */
 function production(app) {
-  if (process.env.NODE_ENV !== "production") {
+  if (process.env.NODE_ENV === "production") {
     const root = require("path").join(__dirname, "../client", "build");
     app.use(express.static(root));
     app.get("*", (req, res) => {
